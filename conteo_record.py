1465b8882103d53c4100b8949369d4bc179365b7
Alexdiruz <80867875+Alexdiruz@users.noreply.github.com>
2023-12-30 12:21:53 -0600

    Update conteo_record.py

--- conteo_record.py
+++ conteo_record.py
@@ -1,17 +1,14 @@
-##Librerías
-
+#Librerías.
 import numpy as np
 import pandas as pd
 
-##Base de datos
-
+#Base de datos.
 team_stats = pd.read_csv('team_stats.csv')
 
-## Función conteo_record
-
-#Función que crea tres columnas: número de partidos ganados ('GW'), partidos perdidos ('GL')
-#y una variable que indica si el equipo tiene un record negativo (0), record neutral (1) o record
-#positivo (1) después de ese partido.
+#Función conteo_record.
+# Función que crea tres columnas: número de partidos ganados ('GW'), partidos perdidos ('GL')
+# y una variable que indica si el equipo tiene un record negativo (0), record neutral (1) o record
+# positivo (1) después de ese partido.
 def conteo_record(equipos):
     equipos['GW'] = equipos['W/L'].eq('W').groupby(equipos['TEAM']).cumsum()
     equipos['GL'] = equipos['W/L'].eq('L').groupby(equipos['TEAM']).cumsum()
@@ -19,5 +16,5 @@ def conteo_record(equipos):
 
 conteo_record(team_stats)
 
-#El siguiente código descarga la nueva base de datos en la computadora como documento csv.
+# El siguiente código descarga la nueva base de datos como csv:
 team_stats.to_csv('team_stats_record.csv', index=False)
