0694f72bbea825b8212e4f8c6916bcff4aeee42a
Alexdiruz <80867875+Alexdiruz@users.noreply.github.com>
2024-01-29 15:22:22 -0600

    Update matriz_correlacion.py

--- matriz_correlacion.py
+++ matriz_correlacion.py
@@ -3,10 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Base de datos.
 team_stats = pd.read_csv('team_stats.csv')
 
-#Función matriz_correlación.
 def matriz_correlacion(dataframe, columnas):
 
     # Se extraen las columnas indicadas del dataframe indicado.
@@ -21,17 +19,14 @@ def matriz_correlacion(dataframe, columnas):
     # Se grafica la matriz mostrando las correlaciones con dos cifras decimales. Se utilizó una paleta de
     #colores secuencial para facilitar la lectura.
     sns.heatmap(matriz_correlacion, annot=True, cmap='seismic', fmt=".2f", linewidths=0.4, vmin=-1, vmax=1)
-
-    # Se le asigna un título al gráfico y se muestra.
     plt.title('Matriz de Correlación')
     plt.show()
 
-#Selección de columnas y llamada de función.
-# Para esta matriz de correlación, se utilizan únicamente las variables númericas.
+#Selección de columnas y activación de función.
+# Para esta matriz de correlación se utilizan únicamente variables numéricas.
 cols = ['MIN', 'PTS', 'FGM', 'FGA', 'FG%', '3PM', '3PA', '3P%','FTM', 'FTA', 'FT%', 'OREB', 'DREB',
         'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF']
 
-# Se utiliza la función con los parámetros correspondientes.
 matriz_correlacion(team_stats, cols)
 
 #Resultados:
