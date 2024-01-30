6049ea41b84532f601214931e5eff0b81c1fc02a
Alexdiruz <80867875+Alexdiruz@users.noreply.github.com>
2024-01-30 14:08:02 -0600

    Update matriz_correlacion.py

--- matriz_correlacion.py
+++ matriz_correlacion.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Base de datos.
 team_stats = pd.read_csv('team_stats.csv')
 
 def matriz_correlacion(dataframe, columnas):
