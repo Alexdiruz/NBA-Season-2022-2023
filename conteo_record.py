da7a3927272a0ed6ccb55926f4cc109aec626e4d
Alexdiruz <80867875+Alexdiruz@users.noreply.github.com>
2024-01-31 01:54:36 -0600

    Update conteo_record.py

--- conteo_record.py
+++ conteo_record.py
@@ -5,6 +5,10 @@ import pandas as pd
 #Base de datos.
 team_stats = pd.read_csv('team_stats.csv')
 
+# Se elimina el primer caracter de la columna 'TEAM' porque en la base de datos original hay un
+# espacio antes del nombre del equipo.
+team_stats['TEAM'] = team_stats['TEAM'].str[1:]
+
 #Función conteo_record.
 # Función que crea tres columnas: número de partidos ganados ('GW'), partidos perdidos ('GL')
 # y una variable que indica si el equipo tiene un record negativo (0), record neutral (1) o record
